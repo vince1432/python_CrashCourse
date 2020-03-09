2ec961915a339030a805c93bd49fb36e5e989bc9
Vince Harresh Rodriguez <vinceharresh@gmail.com>
2020-03-09 10:04:19 +0800

    initial

---
+++ variables.py
@@ -0,0 +1,28 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+# x = 1
+# y = 2.5
+# name = 'John'
+# is_cool = True
+
+# Multiple assignment
+x,y,name,is_cool = (1,2.5,'John',True)
+
+
+a = x + y
+x = str(x)
+
+print(type(x))
