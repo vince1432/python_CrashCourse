acfe4de33937a1d111b09216f86032a4e9633d56
Vince Harresh Rodriguez <vinceharresh@gmail.com>
2020-03-09 17:27:15 +0800

    loops

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 #Create function
--- loops.py
+++ loops.py
@@ -30,6 +30,6 @@ people = ['john', 'Paul', 'Sara', 'Susan']
 # While loops execute a set of statements as long as a condition is true.
 
 count = 0 
-while count not  10:
+while count < 10:
     print(f'Count: {count}')
     count += 1
