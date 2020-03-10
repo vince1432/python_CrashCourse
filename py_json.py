7ec3dbaaba253d8792c41f6a268090b82cb6f03b
Vince Harresh Rodriguez <vinceharresh@gmail.com>
2020-03-10 12:03:38 +0800

    json

--- py_json.py
+++ py_json.py
@@ -1,1 +1,16 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+userJSON = '{"first_name": "John", "last_name": "Doe", "age": 30}'
+
+#Parse to dict
+user = json.loads(userJSON)
+
+# print(user)
+# print(user['age'])
+
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
+carJSON = json.dumps(car)
+
+print(type(carJSON))
